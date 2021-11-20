5bf986766dc052cbf35f995e8787b61cdf933b58
Stark <serj.elessar@gmail.com>
2021-11-20 17:02:23 +0300

    Markup layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+// Font Awesome 5 (free)
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+// project code
 import './styles/style.scss'
